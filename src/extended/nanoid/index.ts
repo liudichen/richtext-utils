92e175beb7f0e5cd570545835459e19fe2a17997
liudichen <liudichen@foxmail.com>
2023-03-02 22:42:22 +0800

    注释

--- src/extended/nanoid/index.ts
+++ src/extended/nanoid/index.ts
@@ -2,6 +2,12 @@ import { customAlphabet } from 'nanoid';
 
 const urlAlphabet = 'useandom26T198340PX75pxJACKVERYMINDBUSHWOLFGQZbfghjklqvwyzrict';
 
+/** Generate secure URL-friendly unique ID.
+By default, the ID will have 21 symbols to have a collision probability similar to UUID v4
+* @param size — Size of the ID. The default size is 21.
+
+* @return — A random string.
+ */
 export const nanoid = customAlphabet(urlAlphabet, 21);
 export {
   customAlphabet,
