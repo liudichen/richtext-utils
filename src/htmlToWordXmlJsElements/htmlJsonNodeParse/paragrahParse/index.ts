1af83106a2ed551d5bf1cf3c257c038de80383b7
iimm <liudichen@foxmail.com>
2024-12-02 18:55:13 +0800

    修复非粘贴情况下加粗等操作导致最终word中文本消失的bug

--- src/htmlToWordXmlJsElements/htmlJsonNodeParse/paragrahParse/index.ts
+++ src/htmlToWordXmlJsElements/htmlJsonNodeParse/paragrahParse/index.ts
@@ -1,39 +1,51 @@
-import type { HtmlJsonNode, ObjectStyle } from '@iimm/shared';
-import type { HtmlJsonNodeParserOptions, HtmlXmlParamsParagrapNode, GetImageStepTwoParamsFn, HtmlXmlParamsTextNode } from '@/types/index';
-import { htmlSpacingSizeToWordSizeNumber, htmlFontSizeToWordFontSizeNumber, splitHtmlMarginString, getFontFamilyFromObjectStyle, htmlColorToWordColor, getLangFromObjectStyle } from '@/utils/index';
+import type { HtmlJsonNode, ObjectStyle } from "@iimm/shared";
+import type {
+  HtmlJsonNodeParserOptions,
+  HtmlXmlParamsParagrapNode,
+  GetImageStepTwoParamsFn,
+  HtmlXmlParamsTextNode,
+} from "@/types/index";
+import {
+  htmlSpacingSizeToWordSizeNumber,
+  htmlFontSizeToWordFontSizeNumber,
+  splitHtmlMarginString,
+  getFontFamilyFromObjectStyle,
+  htmlColorToWordColor,
+  getLangFromObjectStyle,
+} from "@/utils/index";
 
-import { imageHtmlJsonNodeParser } from '../imageParse';
-import { spanHtmlJsonNodeParser } from '../spanParse';
+import { imageHtmlJsonNodeParser } from "../imageParse";
+import { spanHtmlJsonNodeParser } from "../spanParse";
 
 /** 从段落的style属性(已转化成了对象)获取段落格式参数 */
 export const getParagraphParamsFromStyle = (styles: ObjectStyle = {}, onlyHans = true, styleCamelCase = false) => {
-  const data: HtmlXmlParamsParagrapNode = { type: 'p' };
+  const data: HtmlXmlParamsParagrapNode = { type: "p" };
   const keys = Object.keys(styles);
   if (!keys.length) return data;
   if (styles?.pStyle) data.pStyle = styles.pStyle as string;
-  if (keys.includes('listLvl')) data.listLvl = styles.listLvl;
-  if (keys.includes('listNumId')) data.listNumId = styles.listNumId;
+  if (keys.includes("listLvl")) data.listLvl = styles.listLvl;
+  if (keys.includes("listNumId")) data.listNumId = styles.listNumId;
   // === w:pPr -> w:spacing ======
-  const lineHeightStr = styleCamelCase ? 'lineHeight' : 'line-height';
+  const lineHeightStr = styleCamelCase ? "lineHeight" : "line-height";
   if (keys.includes(lineHeightStr)) {
     let lineHeight = styles[lineHeightStr];
     if (+lineHeight) lineHeight = `${Math.round(+lineHeight * 100)}%`;
     data.line = htmlSpacingSizeToWordSizeNumber(lineHeight);
-    if (styles[styleCamelCase ? 'lineHeightRule' : 'line-height-rule'] === 'exactly') data.lineRuleExact = true;
+    if (styles[styleCamelCase ? "lineHeightRule" : "line-height-rule"] === "exactly") data.lineRuleExact = true;
   }
-  const mMtStr = styleCamelCase ? 'msoParaMarginTop' : 'mso-para-margin-top';
+  const mMtStr = styleCamelCase ? "msoParaMarginTop" : "mso-para-margin-top";
   if (keys.includes(mMtStr)) {
     data.beforeLines = htmlSpacingSizeToWordSizeNumber(styles[mMtStr]);
   }
-  const mtStr = styleCamelCase ? 'marginTop' : 'margin-top';
+  const mtStr = styleCamelCase ? "marginTop" : "margin-top";
   if (keys.includes(mtStr)) {
     data.before = htmlSpacingSizeToWordSizeNumber(styles[mtStr]);
   }
-  const mMbStr = styleCamelCase ? 'msoParaMarginBottom' : 'mso-para-margin-bottom';
+  const mMbStr = styleCamelCase ? "msoParaMarginBottom" : "mso-para-margin-bottom";
   if (keys.includes(mMbStr)) {
     data.afterLines = htmlSpacingSizeToWordSizeNumber(styles[mMbStr]);
   }
-  const mbStr = styleCamelCase ? 'marginBottom' : 'margin-bottom';
+  const mbStr = styleCamelCase ? "marginBottom" : "margin-bottom";
   if (keys.includes(mbStr)) {
     data.after = htmlSpacingSizeToWordSizeNumber(styles[mbStr]);
   }
@@ -41,7 +53,7 @@ export const getParagraphParamsFromStyle = (styles: ObjectStyle = {}, onlyHans =
   // ===============
   // ===== w:pPr -> w:rPr -> w:sz/w:szCs
   let fontSize: number;
-  const fsStr = styleCamelCase ? 'fontSize' : 'font-size';
+  const fsStr = styleCamelCase ? "fontSize" : "font-size";
   if (keys.includes(fsStr)) {
     fontSize = htmlFontSizeToWordFontSizeNumber(styles[fsStr]);
     if (fontSize) data.fontSize = fontSize;
@@ -49,16 +61,16 @@ export const getParagraphParamsFromStyle = (styles: ObjectStyle = {}, onlyHans =
   // ============================
   // ===== w:pPr -> w:ind =====
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [ t, r, b, l ] = splitHtmlMarginString(styles.margin).map((item) => htmlSpacingSizeToWordSizeNumber(item));
-  const mMlStr = styleCamelCase ? 'msoParaMarginLeft' : 'mso-para-margin-left';
-  const mlStr = styleCamelCase ? 'marginLeft' : 'margin-left';
+  const [t, r, b, l] = splitHtmlMarginString(styles.margin).map((item) => htmlSpacingSizeToWordSizeNumber(item));
+  const mMlStr = styleCamelCase ? "msoParaMarginLeft" : "mso-para-margin-left";
+  const mlStr = styleCamelCase ? "marginLeft" : "margin-left";
   if (keys.includes(mMlStr)) {
     const value = styles[mMlStr];
     const wordValue = htmlSpacingSizeToWordSizeNumber(value, fontSize);
-    if (typeof value === 'string' && (value.endsWith('gd') || value.endsWith('em'))) {
-      data.leftChars = Math.round(+(value.slice(0, value.length - 2)) * 100);
+    if (typeof value === "string" && (value.endsWith("gd") || value.endsWith("em"))) {
+      data.leftChars = Math.round(+value.slice(0, value.length - 2) * 100);
       if (!keys.includes(mlStr) && !l) {
-        data.left = Math.round(wordValue / 100 * 210);
+        data.left = Math.round((wordValue / 100) * 210);
       }
     } else {
       data.left = wordValue;
@@ -69,15 +81,15 @@ export const getParagraphParamsFromStyle = (styles: ObjectStyle = {}, onlyHans =
   } else if (l) {
     data.left = l;
   }
-  const mMrStr = styleCamelCase ? 'msoParaMarginRight' : 'mso-para-margin-right';
-  const mrStr = styleCamelCase ? 'marginRight' : 'margin-right';
+  const mMrStr = styleCamelCase ? "msoParaMarginRight" : "mso-para-margin-right";
+  const mrStr = styleCamelCase ? "marginRight" : "margin-right";
   if (keys.includes(mMrStr)) {
     const value = styles[mMrStr];
     const wordValue = htmlSpacingSizeToWordSizeNumber(value, fontSize);
-    if (typeof value === 'string' && (value.endsWith('gd') || value.endsWith('em'))) {
-      data.rightChars = Math.round(+(value.slice(0, value.length - 2)) * 100);
+    if (typeof value === "string" && (value.endsWith("gd") || value.endsWith("em"))) {
+      data.rightChars = Math.round(+value.slice(0, value.length - 2) * 100);
       if (!keys.includes(mrStr) && !r) {
-        data.right = Math.round(wordValue / 100 * 210);
+        data.right = Math.round((wordValue / 100) * 210);
       }
     } else {
       data.right = wordValue;
@@ -88,18 +100,18 @@ export const getParagraphParamsFromStyle = (styles: ObjectStyle = {}, onlyHans =
   } else if (r) {
     data.left = r;
   }
-  const indentStr = styleCamelCase ? 'textIndent' : 'text-indent';
+  const indentStr = styleCamelCase ? "textIndent" : "text-indent";
   if (keys.includes(indentStr)) {
     const value = styles[indentStr];
     const wordValue = htmlSpacingSizeToWordSizeNumber(value, fontSize);
     if (wordValue) {
-      if (typeof value === 'string' && (value.endsWith('gd') || value.endsWith('em'))) {
+      if (typeof value === "string" && (value.endsWith("gd") || value.endsWith("em"))) {
         if (wordValue > 0) {
-          data.firstLineChars = Math.round(+(value.slice(0, value.length - 2)) * 100);
-          data.firstLine = Math.round(wordValue / 100 * 210);
+          data.firstLineChars = Math.round(+value.slice(0, value.length - 2) * 100);
+          data.firstLine = Math.round((wordValue / 100) * 210);
         } else {
-          data.hangingChars = Math.round(+(value.slice(0, value.length - 2)) * -100);
-          data.hanging = Math.round(wordValue / 100 * -210);
+          data.hangingChars = Math.round(+value.slice(0, value.length - 2) * -100);
+          data.hanging = Math.round((wordValue / 100) * -210);
         }
       } else {
         if (wordValue > 0) {
@@ -110,26 +122,32 @@ export const getParagraphParamsFromStyle = (styles: ObjectStyle = {}, onlyHans =
       }
     }
   }
-  const mCharIndentStr = styleCamelCase ? 'msoCharIndentCount' : 'mso-char-indent-count';
+  const mCharIndentStr = styleCamelCase ? "msoCharIndentCount" : "mso-char-indent-count";
   if (keys.includes(mCharIndentStr)) {
     const value = +styles[mCharIndentStr];
     if (value > 0) {
       data.firstLineChars = value * 100;
-      if (typeof data.firstLine === 'undefined') data.firstLine = fontSize ? Math.round(value * fontSize * 20) : Math.round(value * 210);
+      if (typeof data.firstLine === "undefined")
+        data.firstLine = fontSize ? Math.round(value * fontSize * 20) : Math.round(value * 210);
     } else if (value < 0) {
       data.hangingChars = Math.round(value * -100);
-      if (typeof data.hanging === 'undefined') data.hanging = fontSize ? Math.round(value * fontSize * -20) : Math.round(value * -210);
+      if (typeof data.hanging === "undefined")
+        data.hanging = fontSize ? Math.round(value * fontSize * -20) : Math.round(value * -210);
     }
   }
   // ========================
   // ====== w:pPr -> w:jc
-  const alignStr = styleCamelCase ? 'textAlign' : 'text-align';
+  const alignStr = styleCamelCase ? "textAlign" : "text-align";
   if (keys.includes(alignStr)) {
     const align = styles[alignStr];
-    if (align === 'right' || align === 'center' || align === 'left') {
+    if (align === "right" || align === "center" || align === "left") {
       data.align = align;
-    } else if (align === 'justify' && (styles[styleCamelCase ? 'textAlignLast' : 'text-align-last'] === 'justify' || styles[styleCamelCase ? 'textJustify' : 'text-justify'] === 'distribute-all-lines')) {
-      data.align = 'distribute';
+    } else if (
+      align === "justify" &&
+      (styles[styleCamelCase ? "textAlignLast" : "text-align-last"] === "justify" ||
+        styles[styleCamelCase ? "textJustify" : "text-justify"] === "distribute-all-lines")
+    ) {
+      data.align = "distribute";
     }
   }
 
@@ -141,7 +159,7 @@ export const getParagraphParamsFromStyle = (styles: ObjectStyle = {}, onlyHans =
     const color = htmlColorToWordColor(styles.color as string);
     if (color) data.color = color;
   }
-  const kernStr = styleCamelCase ? 'msoFontKerning' : 'mso-font-kerning';
+  const kernStr = styleCamelCase ? "msoFontKerning" : "mso-font-kerning";
   if (styles[kernStr]) {
     const kern = htmlSpacingSizeToWordSizeNumber(kernStr);
     if (kern) {
@@ -154,8 +172,20 @@ export const getParagraphParamsFromStyle = (styles: ObjectStyle = {}, onlyHans =
   return data;
 };
 
-export const paragraphHtmlJsonNodeParser = async (node: HtmlJsonNode, getImageStepTwoParamsFn?: GetImageStepTwoParamsFn, options?: HtmlJsonNodeParserOptions) => {
-  const { NODENAME = 'name', TEXTTAG = 'text', TEXTVALUE = 'text', CHILDREN = 'elements', STYLE = 'style', onlyHans = true, styleCamelCase = false } = options || {};
+export const paragraphHtmlJsonNodeParser = async (
+  node: HtmlJsonNode,
+  getImageStepTwoParamsFn?: GetImageStepTwoParamsFn,
+  options?: HtmlJsonNodeParserOptions
+) => {
+  const {
+    NODENAME = "name",
+    TEXTTAG = "text",
+    TEXTVALUE = "text",
+    CHILDREN = "elements",
+    STYLE = "style",
+    onlyHans = true,
+    styleCamelCase = false,
+  } = options || {};
   const { [CHILDREN]: children, [STYLE]: style = {}, [NODENAME]: pTagName } = node;
   const data = [];
   let imgFirst = true;
@@ -168,21 +198,32 @@ export const paragraphHtmlJsonNodeParser = async (node: HtmlJsonNode, getImageSt
       items.push({ ...paragraphParams, type, text });
       if (!data.length) imgFirst = false;
     }
-    if (tagName === 'span') {
+    if (tagName === "span" || ["b", "strong", "em", "i", "u", "s", "sub", "sup"].includes(tagName)) {
       const result = [];
       /** 将段落的字体和字号向下作为默认值传递给下面的span节点 */
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { type, ...parentStyle } = paragraphParams;
-      await spanHtmlJsonNodeParser(child, {}, parentStyle as HtmlXmlParamsTextNode, result, getImageStepTwoParamsFn, pTagName === 'li' ? paragraphParams : undefined, options);
+      await spanHtmlJsonNodeParser(
+        child,
+        {},
+        parentStyle as HtmlXmlParamsTextNode,
+        result,
+        getImageStepTwoParamsFn,
+        pTagName === "li" ? paragraphParams : undefined,
+        options
+      );
       items.push(...result);
       if (!data.length) imgFirst = false;
-    } else if (tagName === 'img') { // 内联图片可以任务是p的子元素
+    } else if (tagName === "img") {
+      // 内联图片可以任务是p的子元素
       const imgNode = await imageHtmlJsonNodeParser(child, getImageStepTwoParamsFn, options);
-      if (imgNode) { items.push(imgNode); }
+      if (imgNode) {
+        items.push(imgNode);
+      }
     }
   }
   // 清除没有文本内容的 text子项
-  items = items.filter((ele) => ele.type !== TEXTTAG || ele[TEXTVALUE] !== '');
+  items = items.filter((ele) => ele.type !== TEXTTAG || ele[TEXTVALUE] !== "");
   if (imgFirst || !data.length) {
     data.push({ ...paragraphParams, items });
   } else {
@@ -190,7 +231,6 @@ export const paragraphHtmlJsonNodeParser = async (node: HtmlJsonNode, getImageSt
   }
   return data;
 };
-
 
 // const ParagraphRelateStyleKeys = [
 //   'line-height', 'line-height-rule',
